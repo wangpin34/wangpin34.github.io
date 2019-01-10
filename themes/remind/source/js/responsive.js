b02df0254fa91402036045ca9919f716b58c1a32
wangpin34 <270280522@qq.com>
2019-01-10 15:16:05 +0800

    style: update

--- themes/remind/source/js/responsive.js
+++ themes/remind/source/js/responsive.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
   $(document).on('click', 'nav i.fa-bars', function(event){
     $('nav ul').toggleClass('visible');
+    if ($('nav ul').hasClass('visible')) {
+      $('document').not('nav ul').one('click', function(event){
+        event.stopPropagation();
+        $('nav ul').removeClass('visible');
+      })
+    }
   })
 })
